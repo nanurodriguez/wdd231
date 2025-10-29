daaa6277812136bf59b72415c68071449d0335bd
nanurodriguez <nancy95rod@gmail.com>
2025-10-28 22:43:26 -0600

    adding the correct JS code toggle to show and hide the link menu in the hamburger button

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,12 @@
 
 // adding event listener to the hamburger button
-const hamButton = document.getElementById('ham-button');
-hamButton.addEventListener('click', () => {
-    hamButton.classList.toggle('show');
+const navButton = document.querySelector('#ham-button');
+const navLinks = document.querySelector('#nav-bar');
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    // toggling the navigation bar visibility;
+    navLinks.classList.toggle('show');
 }); 
+// when the button is clicked, it will add or remove the 'show' class
+// to both the button and the navigation links, allowing for CSS styling
+// to show or hide the navigation menu.
