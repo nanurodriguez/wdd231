4c42c8131e27941ec7fe2b0e9a9c0516ad4135ef
nanurodriguez <nancy95rod@gmail.com>
2025-11-13 02:14:30 -0700

    adding the corresponding js and json files to fetch or get the members displayed in the cards from the directory page

---
+++ chamber/scripts/members.js
@@ -0,0 +1,26 @@
+//This code will be used to display the chamber members on the chamber directory page. It will async fetch and toggle view of these members in the DIRECTORY page.
+
+async function loadMembers() {
+  const response = await fetch('data/members.json');
+  const members = await response.json();
+  displayMembers(members);
+}
+
+function displayMembers(members) {
+  const container = document.querySelector('.members-container');
+  container.innerHTML = members.map(member => `
+    <div class="member-card">
+      <h3>${member.name}</h3>
+      <p>${member.address}</p>
+      <p>${member.phone}</p>
+      <a href="${member.website}" target="_blank">Visit Website</a>
+    </div>
+  `).join('');
+}
+
+document.querySelector('#toggleView').addEventListener('click', () => {
+  document.querySelector('.members-container').classList.toggle('list');
+  document.querySelector('.members-container').classList.toggle('grid');
+});
+
+loadMembers();
