ac1737007b77f4199fce68dce9d6c2981e985cf9
nanurodriguez <nancy95rod@gmail.com>
2025-11-18 11:18:15 -0700

    adding the home or index link to the logo of the website both top and bottom

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,7 +1,7 @@
 //set last modified date - CHAMBER WEBSITE
 const lastModified = new Date(document.lastModified);
 const options = { year: 'numeric', month: 'long', day: 'numeric' };
-document.getElementById('lastModified').textContent = `Last Modified:${lastModified} .${toLocaleDateString('en-US', options)}`;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified.toLocaleDateString('en-US', options)}`;
 
 // Set current year in footer
 const currentYear = new Date().getFullYear();
